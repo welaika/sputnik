4623906699b40ab1041bdce5ffb7e18f2e314fc3
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-12-09 11:47:38 +0100

    Wip

--- lib/sputnik.ex
+++ lib/sputnik.ex
@@ -13,6 +13,19 @@ defmodule Sputnik do
 
   """
   def hello do
-    :world
+    HTTPoison.start
+    response = HTTPoison.get! "https://dev.welaika.com"
+    %HTTPoison.Response{body: body} = response
+    Floki.find(body, "a")
+    |> Floki.attribute("href")
+    |> Enum.each(& get_page/1)
   end
+
+  def get_page(url) do
+    URI.merge("https://dev.welaika.com", url)
+    |> to_string
+    |> HTTPoison.get!
+  end
+
+
 end
