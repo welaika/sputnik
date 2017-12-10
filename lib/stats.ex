bc831ac42544a7477f93f07f325c303e39ee784e
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2017-12-10 13:01:19 +0100

    Verbose output

--- lib/stats.ex
+++ lib/stats.ex
@@ -1,35 +1,69 @@
 defmodule Stats do
   def show(done) do
-    IO.puts ""
+    IO.puts "\n\n"
     print_status_codes(done)
+    IO.puts "\n"
     print_queries_counters(done)
     IO.puts "\n"
     Greetings.byebye
   end
 
   defp print_queries_counters(done) do
-    queries = Enum.map(done, fn({_, _, q}) -> q end)
+    queries_report(counter_queries(done), min_max_queries(done))
+  end
 
-    totals = Enum.reduce(queries, %{}, fn(result, acc) ->
-      Map.merge(acc, result, fn(_, old, new) ->
-        old + new
-      end)
-    end)
+  defp counter_queries(done) do
+    Enum.map(done, fn({_, _, q}) -> q end)
+      |> Enum.reduce(%{}, fn(result, acc) ->
+           Map.merge(acc, result, fn(_, old, new) -> old + new end)
+         end)
+  end
 
-    IO.puts "## Queries ##"
-    Map.keys(totals)
+  defp min_max_queries(done) do
+    Enum.map(done, fn({_, _, q}) -> q end)
+      |> Enum.map(fn(item) -> Map.keys(item) end)
+      |> List.flatten
+      |> Enum.uniq
+      |> Enum.map(fn(key) ->
+           counters = Enum.map(done, fn({_, _, item}) -> item[key] end) |> Enum.filter(& &1)
+           %{key => { Enum.min(counters), Enum.max(counters)}}
+         end)
+      |> Enum.reduce(%{}, fn(result, acc) ->
+           Map.merge(acc, result, fn(_, old, new) -> old + new end)
+         end)
+  end
+
+  defp queries_report(counters, _) when counters == %{}, do: nil
+  defp queries_report(counters, min_max_queries) do
+    IO.puts decorate_title('Queries')
+
+    Map.keys(counters)
       |> Enum.each(fn(query) ->
-        IO.puts "#{query}: #{totals[query]}"
-      end)
+           IO.puts "## query `#{query}` ##"
+           IO.puts "#{counters[query]} result(s)"
+           {minimum, maximum} = min_max_queries[query]
+           IO.puts "Min #{minimum} result(s) per page"
+           IO.puts "Max #{maximum} result(s) per page"
+         end)
   end
 
   defp print_status_codes(done) do
-    IO.puts "## Urls ##"
-    IO.puts "Urls found #: #{Enum.count(done)}"
+    IO.puts(decorate_title('Pages'))
+    IO.puts "Pages found: #{Enum.count(done)}"
     map = Enum.group_by(done, fn({status_code, _, _}) -> status_code end)
+
     Map.keys(map)
       |> Enum.sort
       |> Enum.each(fn(status_code) ->
-        IO.puts "#{status_code}: #{Enum.count(map[status_code])}" end)
+        IO.puts "status_code #{status_code}: #{Enum.count(map[status_code])}"
+        status_code_report(status_code, map[status_code])
+      end)
+  end
+
+  defp status_code_report(status_code, _) when status_code in 0..399, do: nil
+  defp status_code_report(_, pages), do: Enum.each(pages, fn ({_, url, _}) -> IO.puts("- #{url}") end)
+
+  defp decorate_title(title) do
+    "#################### #{title} ####################"
   end
 end
