ddf265c59c34edc3d136bba4fe306c5c53b532d0
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2017-12-10 17:28:07 +0100

    Queries chart

--- static/application.js
+++ static/application.js
@@ -1,4 +1,4 @@
-var make_series = function() {
+var make_status_code_series = function() {
  var statuses = report_data.status_codes;
  var counts = [];
  const reducer = (accumulator, currentValue) => accumulator + currentValue;
@@ -14,11 +14,9 @@ var make_series = function() {
    })
  }
  return series;
-}
+};
 
-var series = make_series();
-
-Highcharts.chart('pie_chart', {
+Highcharts.chart('status_codes_chart', {
   chart: {
     plotBackgroundColor: null,
     plotBorderWidth: null,
@@ -47,6 +45,57 @@ Highcharts.chart('pie_chart', {
   series: [{
     name: 'Status codes',
     colorByPoint: true,
-    data: series
+    data: make_status_code_series()
   }]
+});
+
+var make_query_categories = function() {
+  var queries = report_data.queries;
+  var categories = [];
+  for(query in queries) {
+    categories.push(query);
+  };
+
+  return categories;
+};
+
+var make_query_series = function() {
+  var acc = {total: [], min: [], max: []};
+  var categories = make_query_categories();
+  for(var i = 0; i < categories.length; i++) {
+    var category = categories[i];
+    acc.total.push(report_data.queries[category]);
+    console.log(report_data.min_max);
+    console.log(category);
+    acc.min.push(report_data.min_max[category]['min']);
+    acc.max.push(report_data.min_max[category]['max']);
+  }
+  return [{name: "total", data: acc.total},
+          {name: "min", data: acc.min},
+          {name: "max", data: acc.max}]
+};
+
+Highcharts.chart('queries_chart', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Queries'
+  },
+  xAxis: {
+    categories: make_query_categories(),
+    crosshair: true
+  },
+  yAxis: {
+    min: 0,
+    title: { text: "Queries" }
+  },
+  tooltip: {},
+  plotOptions: {
+    column: {
+      pointPadding: 0.2,
+      borderWidth: 0
+    }
+  },
+  series: make_query_series()
 });
